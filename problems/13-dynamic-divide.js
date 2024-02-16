70ce4d2f2abc3037afab3781e86a0297b05adb56
Jose <josegarciaemail96@gmail.com>
2024-02-15 20:29:39 -0500

    13done

--- problems/13-dynamic-divide.js
+++ problems/13-dynamic-divide.js
@@ -18,7 +18,7 @@ divideByFive(50); // returns 10
 
 ***********************************************************************/
 
-// Your code here 
+dynamicDivide = (divisor) => (num) => num / divisor
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
