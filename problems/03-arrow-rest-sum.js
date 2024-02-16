19af159586f29825b0a3dbde7c9fb6aabe17f3a1
Jose <josegarciaemail96@gmail.com>
2024-02-15 19:12:59 -0500

    3done

--- problems/03-arrow-rest-sum.js
+++ problems/03-arrow-rest-sum.js
@@ -11,8 +11,15 @@ arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
 arrowRestSum(0); // => 0
 ***********************************************************************/
 
-// Your code here 
+  const arrowRestSum = (...nums) => {
+    let count = 0;
 
+    for(let i = 0; i < nums.length;i++){
+        let el = nums[i];
+        count += el;
+    }
+        return count;
+  }
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
