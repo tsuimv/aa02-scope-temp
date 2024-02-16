946b722545bd266af02170fd8416a82dd759269a
Jose <josegarciaemail96@gmail.com>
2024-02-15 19:35:28 -0500

    7done

--- problems/07-set-time-out.js
+++ problems/07-set-time-out.js
@@ -18,7 +18,11 @@ funcTimer(5000, partyFunc); // in 5 seconds prints: "Party time!"
 
 ***********************************************************************/
 
-// Your code here 
+ function funcTimer(time,func){
+  return setTimeout(()=> {
+    func()
+  },time)
+ }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
